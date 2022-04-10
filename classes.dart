b2bd9cbf96992811f230dccf4841077be9a57a6f
rafaella-nunes <rafaella.rosendo@arapiraca.ufal.br>
2022-04-10 18:18:27 -0300

    added constructor

--- classes.dart
+++ classes.dart
@@ -1,11 +1,12 @@
 void main(){
-  var person = new Person();
-  person.firstName = 'Rafaella';
+  var person = new Person('Rafaella');
   person.printName();
 }
 
 class Person{
-  String firstName = '';
+  String firstName;
+
+  Person(this.firstName);
 
   printName(){
     print(this.firstName);
