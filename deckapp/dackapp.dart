748ebfb75c13c988b15eebe7ad9aa602b1db2515
Rafaella Nunes <rafaella.rosendo@arapiraca.ufal.br>
2022-04-12 12:00:33 -0300

    estudo de estruturas de repetição

--- deckapp/dackapp.dart
+++ deckapp/dackapp.dart
@@ -6,12 +6,21 @@ class Deck {
   List<Card> cards = [];
 
   Deck(){
-    
+    var ranks = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', "Jack", 'Queen', 'King'];
+    var suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'];
+
+    for (var suit in suits){
+      for(var rank in ranks){
+        var card = new Card(rank, suit);
+        cards.add(card);
+      }
+    }
   }
 }
 
 class Card{
-  String rank;
-  String suit;
+  String rank = '';
+  String suit = '';
 
+  Card(this.rank, this.suit);
 }
