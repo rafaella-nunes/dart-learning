ecc6a5a87a1ceba419575f9701273ef59250a3ba
rafaella-nunes <rafaella.rosendo@arapiraca.ufal.br>
2022-04-12 17:58:39 -0300

    Bug fixes and shuffle fuction impletation

--- deckapp/dackapp.dart
+++ deckapp/dackapp.dart
@@ -1,26 +1,54 @@
-void main(){
-  
+void main() {
+  var deck = new Deck();
+  deck.shuffle();
+  print(deck);
 }
 
 class Deck {
   List<Card> cards = [];
 
-  Deck(){
-    var ranks = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', "Jack", 'Queen', 'King'];
+  Deck() {
+    var ranks = [
+      'Ace',
+      'Two',
+      'Three',
+      'Four',
+      'Five',
+      'Six',
+      'Seven',
+      'Eight',
+      'Nine',
+      'Ten',
+      "Jack",
+      'Queen',
+      'King'
+    ];
     var suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'];
 
-    for (var suit in suits){
-      for(var rank in ranks){
+    for (var suit in suits) {
+      for (var rank in ranks) {
         var card = new Card(rank, suit);
         cards.add(card);
       }
     }
   }
+
+  toString() {
+    return cards.toString();
+  }
+
+  shuffle() {
+    cards.shuffle();
+  }
 }
 
-class Card{
+class Card {
   String rank = '';
   String suit = '';
 
   Card(this.rank, this.suit);
+
+  toString() {
+    return '$rank of $suit';
+  }
 }
